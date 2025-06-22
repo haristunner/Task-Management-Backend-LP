770358abd1bf26293f63c3db959157afab9932ce
haristunner <hari17prasathb@gmail.com>
2025-06-22 11:47:14 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { PORT } = require("./config/var");
+const app = express();
+
+app.listen(process.env.X_ZOHO_CATALYST_LISTEN_PORT || PORT, () => {
+  console.log(
+    `Server running on port ${process.env.X_ZOHO_CATALYST_LISTEN_PORT || PORT}`
+  );
+});
